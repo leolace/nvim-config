e6a6b5c023c98f5e599190d23cce377583436207
leeool <lgonsalez02@gmail.com>
2024-03-16 12:33:00 -0300

    update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -81,9 +81,9 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fp", telescope.extensions.projects.projects, {})
 vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {})
-vim.keymap.set("n", "<leader>fi", builtin.lsp_definitions, {})
+vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, {})
 vim.keymap.set("n", "<leader>fc", builtin.colorscheme, { desc = "select a coloscheme" })
-vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
 
 
 -- NVIMTREE
